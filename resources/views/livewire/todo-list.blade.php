20e7201cef702c5ea370d2959f82fb0be38ef92b
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-07-26 16:54:27 +0530

    Todo done

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    @include('livewire.includes.create-todo-box')
+    @include('livewire.includes.search-box')
+    
+    <div id="todos-list">
+        @foreach ($todos as $todo)
+            @include('livewire.includes.todo-card')
+        @endforeach
+
+        <div class="my-2">
+            {{ $todos->links() }}
+        </div>
+    </div>
 </div>

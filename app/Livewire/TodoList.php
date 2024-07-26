cf89de87f76ff17d8dbe7e11f6c5963551ef0599
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-07-26 12:33:07 +0530

    #2: Helper functions

---
+++ app/Livewire/TodoList.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Todo;
+use Livewire\Component;
+use Livewire\Attributes\Rule;
+use Livewire\WithPagination;
+use App\Helpers\TodoHelper;
+use Illuminate\Support\Facades\Session;
+
+class TodoList extends Component
+{
+    use WithPagination;
+
+    
+    #[Rule('required|min:3|max:50')]
+    public $name;
+    public $search;
+    public $editingTodoID;
+    #[Rule('required|min:3|max:50')]
+    public $editingTodoName;
+    
+    protected $todoHelper;
+
+    public function mount(){
+        $this->todoHelper = new TodoHelper();
+    }
+    
+    public function create(){
+        // Check if $todoHelper is not set and initialize it
+        if (!$this->todoHelper) {
+            $this->todoHelper = new TodoHelper();
+        }
+
+        // validate
+        $validated = $this->validateOnly('name');
+        //create the todo
+        $this->todoHelper->createTodo($validated['name']);
+        // clear the input
+        $this->reset('name');
+        //send the flash message
+        Session::flash('success', 'Saved.');
+    }
+
+    // public function delete(Todo $todo){
+    //     $todo->delete();
+    // }
+
+    public function toggle($todoID){
+        $todo = Todo::find($todoID);
+        $todo->completed = !$todo->completed;
+        $todo->save();
+    }
+
+    public function delete($todoID){
+        TodoHelper::deleteTodo($todoID);
+    }
+
+    public function edit($todoID){
+        $this->editingTodoID = $todoID;
+        $this->editingTodoName = '';
+        TodoHelper::editTodo($this->editingTodoID, $this->editingTodoName);
+    }
+
+    public function cancelEdit(){
+        $this->reset('editingTodoID', 'editingTodoName');
+    }
+    public function update(){
+        $this->validateOnly('editingTodoName');
+        TodoHelper::updateTodo($this->editingTodoID, $this->editingTodoName);
+            $this->cancelEdit();
+    }
+
+    public function render(){
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(3)
+        ]);
+    }
+}
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('todo') }}" :active="request()->routeIs('todo-welcome')">
+                        {{ __('Todo') }}
+                    </x-nav-link>
                 </div>
             </div>
 

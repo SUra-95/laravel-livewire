1ab48c0fb73b53018f3512465285e0733a840d8f
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-07-30 10:21:24 +0530

    Open the create modal with alphineJs

--- resources/views/livewire/includes/create-todo-box.blade.php
+++ resources/views/livewire/includes/create-todo-box.blade.php
@@ -27,16 +27,15 @@
                 </form> --}}
 
                 <!-- Modal -->
-                <div>
+                <div x-data="{ isOpen: false }">
 
-                    <div x-data="{ isOpen:  $wire.entangle('modalOpen')}">
-                        <div
-                            x-show="isOpen"
-                            class=" flex fixed inset-0 z-40 min-h-full overflow-y-auto overflow-x-hidden transition items-center justify-center"
-                            >
-                            <div aria-hidden="true" class="fixed inset-0 w-full h-full bg-black/80 cursor-pointer" @click="isOpen = false">
+                    <div>
+                        <div x-show="isOpen"
+                            class=" flex fixed inset-0 z-40 min-h-full overflow-y-auto overflow-x-hidden transition items-center justify-center">
+                            <div aria-hidden="true" class="fixed inset-0 w-full h-full bg-black/80 cursor-pointer"
+                                @click="isOpen = false">
                             </div>
-                
+
                             <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
                                 <!-- Modal content -->
                                 <div class="relative p-4 bg-gray-200 rounded-lg shadow dark:bg-gray-100 sm:p-5">
@@ -49,8 +48,8 @@
                                         <button type="button" @click="isOpen = false"
                                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-100 dark:hover:text-black"
                                             data-modal-toggle="defaultModal">
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                                xmlns="http://www.w3.org/2000/svg">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor"
+                                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd"
                                                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                                                     clip-rule="evenodd"></path>
@@ -62,17 +61,18 @@
                                     <div class="grid gap-4 mb-4 sm:grid-cols-2">
                                         <div class="sm:col-span-2">
                                             <label for="name"
-                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Todo Name</label>
+                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Todo
+                                                Name</label>
                                             <input type="text" name="name" id="name" wire:model="name"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                                 placeholder="Type Todo title">
                                             @error('name')
                                                 <p class="text-red-600">{{ $message }}</p>
                                             @enderror
-                
+
                                         </div>
                                     </div>
-                                    <button type="button" wire:click.prevent="create" 
+                                    <button type="button" wire:click.prevent="create"
                                         class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                         <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                                             xmlns="http://www.w3.org/2000/svg">
@@ -83,16 +83,18 @@
                                         Create Todo
                                     </button>
                                     @if (session('success'))
-                        <span class="text-green-500 text-xs">{{ session('success') }}</span>
-                    @endif
+                                        <span class="text-green-500 text-xs">{{ session('success') }}</span>
+                                    @endif
                                 </div>
                             </div>
                         </div>
                     </div>
-    
-    
+
+
                     <div class="flex justify-center items-center">
-                        <button wire:click="createModalOpen" 
+                        <button {{-- wire:click="isOpen= true" 
+                        x-on:click="isOpen : true"  --}} 
+                        x-on:click="isOpen = true"
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Create +
                         </button>
